cfe035b062f7d19e680f46c9165facd31046825b
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-12-01 19:08:50 +0530

    updated

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 8000;
 const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-// the __dirname is the current directory from where the script is running
+// the __dirname is the current dir
+ectory from where the script is running
 app.use(express.static(__dirname));
 
+// send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`server started at port : ${port}`);
-});
-
+app.listen(port);
